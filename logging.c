fb3acc08bdc4290ff8dbd81a3d6d6b1bb45735b4
Torphedo <73564623+Torphedo@users.noreply.github.com>
2023-01-26 21:32:16 -0500

    Add option to disable all printing

--- logging.c
+++ logging.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdarg.h>
+#include <stdbool.h>
 
 #include "logging.h"
 
 #ifdef _WIN32
 #include <Windows.h>
-unsigned short ansi_enabled = 0;
 
 // Enables ANSI escape codes on Windows
-void enable_win_ansi()
+unsigned short enable_win_ansi()
 {
     DWORD prev_console_mode;
     HANDLE console_handle = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -16,37 +16,51 @@ void enable_win_ansi()
         GetConsoleMode(console_handle , (LPDWORD) &prev_console_mode);
         if (prev_console_mode != 0) {
             SetConsoleMode(console_handle , prev_console_mode | ENABLE_VIRTUAL_TERMINAL_PROCESSING | ENABLE_PROCESSED_OUTPUT);
-            ansi_enabled = 1;
+            return 1;
         }
     }
+	return 0;
+}
+#endif
+
+static bool printing_enabled = true;
+
+void enable_logging()
+{
+	printing_enabled = true;
 }
 
-#else
-unsigned short ansi_enabled = 1;
-#endif
+void disable_logging()
+{
+	printing_enabled = false;
+}
 
 void log_error(print_type type, char* format_str, ...)
 {
     #ifdef _WIN32
+	static unsigned short ansi_enabled = 0;
     if (ansi_enabled == 0)
     {
-        enable_win_ansi();
+        ansi_enabled = enable_win_ansi();
     }
     #endif
-    switch (type) {
-    case INFO:
-        printf("[\033[32mINFO\033[0m] "); // Green
-        break;
-    case WARNING:
-        printf("[\033[33mWARNING\033[0m] "); // Yellow
-        break;
-    case CRITICAL:
-        printf("[\033[31mCRITICAL\033[0m] "); // Red
-        break;
-    case DEBUG:
-        printf("[\033[34mDEBUG\033[0m] "); // Blue
-        break;
-    }
+	if (printing_enabled)
+	{
+		switch (type) {
+		case INFO:
+			printf("[\033[32mINFO\033[0m] "); // Green
+			break;
+		case WARNING:
+			printf("[\033[33mWARNING\033[0m] "); // Yellow
+			break;
+		case CRITICAL:
+			printf("[\033[31mCRITICAL\033[0m] "); // Red
+			break;
+		case DEBUG:
+			printf("[\033[34mDEBUG\033[0m] "); // Blue
+			break;
+		}
+	}
 
     va_list arg_list;
     va_start(arg_list, format_str);
--- logging.h
+++ logging.h
@@ -7,6 +7,11 @@ typedef enum print_type {
     DEBUG = 3
 }print_type;
 
+// These toggle an internal bool value, when false
+// it makes all calls to log_error() inert.
+void enable_logging();
+void disable_logging();
+
 // Usage is identical to printf(), except that you need to pass in a value
 // from the above enum to control the color / text that prefixes your message
 void log_error(print_type type, char* format_str, ...);
